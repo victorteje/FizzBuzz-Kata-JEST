5f07172e4d5234c0c5cae625287722050f88e7ad
victorteje <vteje2@gmail.com>
2023-06-19 11:51:07 +0200

    created function

---
+++ fizzBuzz.js
@@ -0,0 +1,25 @@
+const fizzBuzz = () => {
+
+    let i = 1;
+    
+    for (i; i <= 15; i++) {
+
+        if (i % 3 == 0) {
+            console.log("Fizz");
+        }
+        
+        else if (i % 5 == 0) {
+            console.log("Buzz");
+        }
+
+        else if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        }
+        
+        else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz();
