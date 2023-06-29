120b052ecb071549be8a4e185d52c084a82abb3b
victorteje <vteje2@gmail.com>
2023-06-29 20:06:50 +0200

    added 4th test

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,6 +1,10 @@
 const { fizzBuzz } = require('./fizzBuzz');
 
 describe('FizzBuzz', () => {
+    test('should return FizzBuzz', () => {
+        let n = 15;
+        expect(fizzBuzz(n)).toBe("FizzBuzz");
+    });
     test('should return Fizz', () => {
         let n = 3;
         expect(fizzBuzz(n)).toBe("Fizz");
@@ -9,8 +13,8 @@ describe('FizzBuzz', () => {
         let n = 5;
         expect(fizzBuzz(n)).toBe("Buzz");
     });
-    test('should return FizzBuzz', () => {
-        let n = 15;
-        expect(fizzBuzz(n)).toBe("FizzBuzz");
+    test('should return 1', () => {
+        let n = 1;
+        expect(fizzBuzz(n)).toBe(1);
     });
 });
