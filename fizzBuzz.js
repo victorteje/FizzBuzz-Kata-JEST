93bb6f60e9f5f912a82636fab75756ad64990c4e
victorteje <vteje2@gmail.com>
2023-06-30 19:08:43 +0200

    cleaned code

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -29,37 +29,3 @@ const fizzBuzz = () => {
 fizzBuzz ();
 
 module.exports = { fizzBuzz };
-
-
-
-// *** PREVIOUS VERSION ***
-
-// const fizzBuzz = (n) => {
-   
-//     for (let i = 1; i <= n; i++) {
-
-//         if (i % 3 === 0 && i % 5 === 0) {
-//             console.log("FizzBuzz");
-//             return "FizzBuzz";
-//         }
-
-//         else if (i % 3 === 0) {
-//             console.log("Fizz");
-//             return "Fizz";
-//         }
-        
-//         else if (i % 5 === 0) {
-//             console.log("Buzz");
-//             return "Buzz";
-//         }
-
-//         else {
-//             console.log(i);
-//             return i;
-//         }
-//     }
-// }
-
-// fizzBuzz(15);
-
-// module.exports = { fizzBuzz };
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -14,28 +14,3 @@ describe('FizzBuzz', () => {
         expect(fizzBuzz()[0]).toBe(1);
     });
 });
-
-
-
-// *** PREVIOUS VERSION ***
-
-// const { fizzBuzz } = require('./fizzBuzz');
-
-// describe('FizzBuzz', () => {
-//     test('should return FizzBuzz', () => {
-//         let n = 15;
-//         expect(fizzBuzz(n)).toBe("FizzBuzz");
-//     });
-//     test('should return Fizz', () => {
-//         let n = 3;
-//         expect(fizzBuzz(n)).toBe("Fizz");
-//     });
-//     test('should return Buzz', () => {
-//         let n = 5;
-//         expect(fizzBuzz(n)).toBe("Buzz");
-//     });
-//     test('should return 1', () => {
-//         let n = 1;
-//         expect(fizzBuzz(n)).toBe(1);
-//     });
-// });
