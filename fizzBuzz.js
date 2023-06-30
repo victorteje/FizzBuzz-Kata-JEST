4de3f87e76e2f62a82a31d78dd39592eb0e43fc7
victorteje <vteje2@gmail.com>
2023-06-30 18:48:37 +0200

    trying array version

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,29 +1,65 @@
-const fizzBuzz = (n) => {
-   
-    for (let i = 1; i <= n; i++) {
+const fizzBuzz = () => {
+    const n = [];
 
-        if (i % 3 === 0 && i % 5 === 0) {
+    for (let i = 0; i<= 14; i++) {
+        n[i] = i + 1;
+
+        if (n[i] % 3 === 0 && n[i] % 5 === 0) {
             console.log("FizzBuzz");
-            return "FizzBuzz";
+            n[i] = "FizzBuzz";
         }
 
-        else if (i % 3 === 0) {
+        else if (n[i] % 3 === 0) {
             console.log("Fizz");
-            // return "Fizz";
+            n[i] = "Fizz";
         }
         
-        else if (i % 5 === 0) {
+        else if (n[i] % 5 === 0) {
             console.log("Buzz");
-            // return "Buzz";
+            n[i] = "Buzz";
         }
 
         else {
-            console.log(i);
-            // return i;
-        }
+            console.log(n[i]);
+        } 
     }
+    return n;
 }
 
-fizzBuzz(15);
+fizzBuzz ();
 
 module.exports = { fizzBuzz };
+
+
+
+// *** PREVIOUS VERSION ***
+
+// const fizzBuzz = (n) => {
+   
+//     for (let i = 1; i <= n; i++) {
+
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//             return "FizzBuzz";
+//         }
+
+//         else if (i % 3 === 0) {
+//             console.log("Fizz");
+//             return "Fizz";
+//         }
+        
+//         else if (i % 5 === 0) {
+//             console.log("Buzz");
+//             return "Buzz";
+//         }
+
+//         else {
+//             console.log(i);
+//             return i;
+//         }
+//     }
+// }
+
+// fizzBuzz(15);
+
+// module.exports = { fizzBuzz };
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,20 +1,41 @@
 const { fizzBuzz } = require('./fizzBuzz');
 
 describe('FizzBuzz', () => {
-    test('should return FizzBuzz', () => {
-        let n = 15;
-        expect(fizzBuzz(n)).toBe("FizzBuzz");
+    test('should return "Fizz" for multiples of 3', () => {
+        expect(fizzBuzz()[2]).toBe("Fizz");
     });
-    test('should return Fizz', () => {
-        let n = 3;
-        expect(fizzBuzz(n)).toBe("Fizz");
+    test('should return "Buzz" for multiples of 5', () => {
+        expect(fizzBuzz()[4]).toBe("Buzz");
     });
-    test('should return Buzz', () => {
-        let n = 5;
-        expect(fizzBuzz(n)).toBe("Buzz");
+    test('should return "FizzBuzz" for multiples of both 3 and 5', () => {
+        expect(fizzBuzz()[14]).toBe("FizzBuzz");
     });
-    test('should return 1', () => {
-        let n = 1;
-        expect(fizzBuzz(n)).toBe(1);
+    test('should return a number if neither a multiple of 3 nor 5', () => {
+        expect(fizzBuzz()[0]).toBe(1);
     });
 });
+
+
+
+// *** PREVIOUS VERSION ***
+
+// const { fizzBuzz } = require('./fizzBuzz');
+
+// describe('FizzBuzz', () => {
+//     test('should return FizzBuzz', () => {
+//         let n = 15;
+//         expect(fizzBuzz(n)).toBe("FizzBuzz");
+//     });
+//     test('should return Fizz', () => {
+//         let n = 3;
+//         expect(fizzBuzz(n)).toBe("Fizz");
+//     });
+//     test('should return Buzz', () => {
+//         let n = 5;
+//         expect(fizzBuzz(n)).toBe("Buzz");
+//     });
+//     test('should return 1', () => {
+//         let n = 1;
+//         expect(fizzBuzz(n)).toBe(1);
+//     });
+// });
